d40bc02b53bfd3d9fa45325f87cd4b848fc3f9bd
Robert <r.niyonkuru@alustudent.com>
2026-09-03 17:45:12 +0200

    feat: add compilation scripts and size demonstration program to hello_world directory

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,12 +4,14 @@ int main(void)
 
 {
   char c = 'A';
-  int i = 42;
+  int i = '42';
   float f = 3.14f;
   double d = 3.14159;
 
-  printf("Size of an int: %zu\n", sizeof(int));
-  printf("Size of a char: %c\n", sizeof(char));
-  printf("Size of a float: %f\n", sizeof(float));
-  printf("Size of a long int: %d\n", sizeof(char));
+  printf("Size of a char: %c byte(s)\n", sizeof(char));
+  printf("Size of an int: %zu byte(s)\n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(char));
+  printf("Size of a long long int: %d byte(s)\n", sizeof(char));
+  printf("Size of a float: %f byte(s)\n", sizeof(float));
+  return (0);
 }
