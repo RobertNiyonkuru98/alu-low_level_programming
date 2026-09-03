6c5a9cb8a9e2cde6dfaae95e91362cec0faec63b
Robert <r.niyonkuru@alustudent.com>
2026-09-03 18:32:36 +0200

    bug: fourth fix  attempt by including the main.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * A program that prints a string using puts
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-  char str['Programming is like building a multilingual puzzle'];
-  puts(str);
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-  char c = 'A';
-  int i = '42';
-  float f = 3.14f;
-  double d = 3.14159;
-
-  printf("Size of a char: %c byte(s)\n", sizeof(char));
-  printf("Size of an int: %zu byte(s)\n", sizeof(int));
-  printf("Size of a long int: %d byte(s)\n", sizeof(char));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(char));
-  printf("Size of a float: %f byte(s)\n", sizeof(float));
-  return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
 }
