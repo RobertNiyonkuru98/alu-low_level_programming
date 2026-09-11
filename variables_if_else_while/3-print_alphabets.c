b154418435fb32c3cde7cd80936fbb9021d4e593
Robert <r.niyonkuru@alustudent.com>
2026-09-11 22:15:21 +0200

    feat: add C programs for last digit evaluation and alphabet printing

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase and
- *uppercase followed by a new line.
+ * main - Prints the alphabet in lowercase and uppercase
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-  char c;
+int main(void)
+{
+	char c;
 
-  for (c = 'a'; c <= 'z'; c++)
-    putchar(c);
-  for (c = 'A'; c <= 'Z'; c++)
-    putchar(c);
-  putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
