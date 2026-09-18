cead50277ad5ad8db89869b183b557e07af0d3cc
Robert <r.niyonkuru@alustudent.com>
2026-09-18 15:09:14 +0200

    feat: add print_sign function to print and return the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
  * print_sign - prints +, 0 or -
  * @n: The number has to either be positive,
@@ -10,13 +9,13 @@
  */
 int print_sign(int n) {
   if (n > 0) {
-    putchar('+');
+    printf("+");
     return (1);
   } else if (n < 0) {
-    putchar('-');
+    printf("-");
     return (-1);
   } else {
-    putchar('0');
+    printf("0");
     return (0);
   }
 }
